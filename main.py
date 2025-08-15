7a0ac994ff371bd8e13cb403d20c557f4df0e454
ZomboySoul <bytechsuppor@gmail.com>
2025-08-15 00:16:39 -0300

    Refactor main.py docstring and add comment for clarity

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+"""
+main.py
+
+Archivo principal de ejecución del juego
+
+Inicia la aplicación llamando al menú principal.
+"""
+
 from ui.menu import menu_principal
 
+
 if __name__ == "__main__":
-   menu_principal()
+    # Ejecuta el menú principal del juego
+    menu_principal()
